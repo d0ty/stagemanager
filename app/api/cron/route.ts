5b0ed15838eada1aa8e11ab7a88f44c801e8be96
d0ty <hp.raszpi@gmail.com>
2026-04-30 16:41:20 +0200

    fixed google drive

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -3,30 +3,32 @@ import { getGoogleAuthClient } from "@/lib/gdrive";
 export async function GET() {
   const auth = await getGoogleAuthClient();
 
-  await fetch(
-    `https://api.vercel.com/v1/edge-config/${new URL(process.env.EDGE_CONFIG!).pathname.split("/")[1]}/items`,
-    {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.VERCEL_TOKEN}`,
+  console.log(
+    await (await fetch(
+      `https://api.vercel.com/v1/edge-config/${new URL(process.env.EDGE_CONFIG!).pathname.split("/")[1]}/items`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.VERCEL_TOKEN}`,
+        },
+        body: JSON.stringify({
+          items: [
+            {
+              operation: "update",
+              key: "access_token",
+              value: auth.credentials.access_token,
+            },
+            {
+              operation: "update",
+              key: "refresh_token",
+              value: auth.credentials.refresh_token,
+            },
+          ],
+        }),
       },
-      body: JSON.stringify({
-        items: [
-          {
-            operation: "update",
-            key: "access_token",
-            value: auth.credentials.access_token,
-          },
-          {
-            operation: "update",
-            key: "refresh_token",
-            value: auth.credentials.refresh_token,
-          },
-        ],
-      }),
-    },
-  );
+    ),
+  ).text());
 
   return new Response("OK", { status: 200 });
 }
