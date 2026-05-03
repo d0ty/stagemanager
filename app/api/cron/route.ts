d08a2e02d4771b1f94c716b5075db2d60e8c3f64
d0ty <hp.raszpi@gmail.com>
2026-05-03 16:42:24 +0200

    fixed error in gdrive cred update fix

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -2,33 +2,31 @@ import { getGoogleAuthClient } from "@/lib/gdrive";
 
 export async function GET() {
   const auth = await getGoogleAuthClient();
-
-  console.log(
-    await (await fetch(
-      `https://api.vercel.com/v1/edge-config/${new URL(process.env.EDGE_CONFIG!).pathname.split("/")[1]}/items`,
-      {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.VERCEL_TOKEN}`,
-        },
-        body: JSON.stringify({
-          items: [
-            {
-              operation: "update",
-              key: "access_token",
-              value: auth.credentials.access_token,
-            },
-            {
-              operation: "update",
-              key: "refresh_token",
-              value: auth.credentials.refresh_token,
-            },
-          ],
-        }),
+  const updateResp = await fetch(
+    `https://api.vercel.com/v1/edge-config/${new URL(process.env.EDGE_CONFIG!).pathname.split("/")[1]}/items`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.VERCEL_TOKEN}`,
       },
-    ),
-  ).text());
+      body: JSON.stringify({
+        items: [
+          {
+            operation: "update",
+            key: "access_token",
+            value: auth.credentials.access_token,
+          },
+          {
+            operation: "update",
+            key: "refresh_token",
+            value: auth.credentials.refresh_token,
+          },
+        ],
+      }),
+    },
+  );
+  console.log(await updateResp.text());
 
   return new Response("OK", { status: 200 });
 }
